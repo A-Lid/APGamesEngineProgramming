0236456832f163fab366796f67947b482a6aec54
MMC198 <MMC198@ME-330-F65556D3.isdads.salford.ac.uk>
2018-10-03 11:53:07 +0100

    Hi Aled

---
+++ EclipseWorkingDirectory/JavaTesting2/src/Main.java
@@ -0,0 +1,22 @@
+import processing.core.PApplet;
+
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Aled Comment on your own code please. Thank you.
+		PApplet.main("Main");
+	}
+	public void setup() {
+		background(0);
+	}
+	public void draw() {
+		
+		
+	}
+	public void settings() {
+		size(640, 400);
+	}
+
+}
