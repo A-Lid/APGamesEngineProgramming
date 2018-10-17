a5155aaa0b409148b2007ef582029cc13fe8268b
MMC198 <AledJPink@gmail.com>
2018-10-17 13:01:42 +0100

    IntelliJ

--- EclipseWorkingDirectory/JavaTesting2/src/GameObject.java
+++ EclipseWorkingDirectory/JavaTesting2/src/GameObject.java
@@ -2,9 +2,10 @@ import processing.core.PApplet;
 
 public abstract class GameObject {
 
+	//this is for the GameObject's Name
 	public String name;
 	
-	
+	//Sets Processing to the parent.
 	PApplet parent;
 	public GameObject(){}
 	public GameObject(PApplet p){
@@ -13,5 +14,7 @@ public abstract class GameObject {
 	
 	public abstract void update();
 	public abstract void render();
+	
+	//abstract class means that it is only here for other classes to inherit from
 }
 
--- EclipseWorkingDirectory/JavaTesting2/src/Main.java
+++ EclipseWorkingDirectory/JavaTesting2/src/Main.java
@@ -1,22 +1,20 @@
 import processing.core.PApplet;
-
-
-public class Main extends PApplet{
+import simple_platformer.Launcher;
+public class Main extends PApplet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Aled Comment on your own code please. Thank you.
 		PApplet.main("Main");
 	}
+	Launcher launcher;
 	public void setup() {
 		background(0);
+		launcher = new Launcher(this);
+		launcher.StartGame();
 	}
 	public void draw() {
-		
-		
+		launcher.UpdateAll();
 	}
 	public void settings() {
-		size(640, 400);
+		size(640,480);
 	}
-
 }
